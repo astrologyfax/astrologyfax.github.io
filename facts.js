d0d646c52fd739b9eea07e696a6b36b26dcd7cd9
astrologyfax <47753594+astrologyfax@users.noreply.github.com>
2019-02-18 14:49:42 -0600

    Update facts.js

--- facts.js
+++ facts.js
@@ -5,5 +5,6 @@ $(document).ready(function() {
 	if (d == 18 && m == 1 && y == 2019) {
 		document.getElementById("fact").textContent = "Pisces hate when they get a text saying \"Wussup stranger\"... Like for real!!! phones work both ways";
 		document.getElementById("fact-source").href = "https://21facts.net/interesting-facts-about-pisces/";
+		document.getElementById("new1").textContent = "Happy Birthday Michelle!";
 	}
 });
