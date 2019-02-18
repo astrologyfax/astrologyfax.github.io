2b80ad6e4d7842ab25ee8b12fa5f31b2e95da0a2
astrologyfax <47753594+astrologyfax@users.noreply.github.com>
2019-02-18 17:28:20 -0600

    Update facts.js

--- facts.js
+++ facts.js
@@ -9,23 +9,28 @@ $(document).ready(function() {
 	var m = new Date().getMonth();
 	var d = new Date().getDate();
 	if (d == 18 && m == 1 && y == 2019) {
-		document.getElementById("fact").textContent = "The longest astrological drought at 206 years and counting is currently being endured by Aries. Thomas Jefferson (April 13) is the last and only elected president born under the sign of The Ram, when he was reelected in 1804.";
+		document.getElementById("fact").textContent = "Verifiable Fact: The longest astrological drought at 206 years and counting is currently being endured by Aries. Thomas Jefferson (April 13) is the last and only elected president born under the sign of The Ram, when he was reelected in 1804.";
 		document.getElementById("fact-source").href = "http://editions.lib.umn.edu/smartpolitics/2010/02/15/presidents-day-special-the-ast/";
-		document.getElementById("new1").textContent = "It's President's Day. Hi Nora";
+		document.getElementById("new1").textContent = "It's President's Day.";
 	}
 	if (d == 19 && m == 1 && y == 2019) {
 		document.getElementById("fact").textContent = "Verifiable Fact: Pisces hate when they get a text saying \"Wussup stranger\"... Like for real!!! phones work both ways";
 		document.getElementById("fact-source").href = "https://21facts.net/interesting-facts-about-pisces/";
 		document.getElementById("new1").textContent = "Happy Birthday Michelle!";
 	}
+	if (d == 11 && m == 5 && y == 2019) {
+		document.getElementById("fact").textContent = "Verifiable Fact: \"Alice\" is an Aquarius";
+		document.getElementById("fact-source").href = "https://www.instagram.com/noraabravos/";
+		document.getElementById("new1").textContent = "Happy Birthday Alice!";
+	}
 	if (d == 2 && m == 6 && y == 2019) {
 		document.getElementById("fact").textContent = "Verifiable Fact: An analysis of data from the FBI found that Cancers are the most frequently arrested of all the zodiac signs, and their crimes are often more serious too.";
 		document.getElementById("fact-source").href = "https://bestlifeonline.com/crazy-horoscope-facts/";
 		document.getElementById("new1").textContent = "Happy Birthday Ryan!";
 	}
-	if (d == 11 && m == 5 && y == 2019) {
-		document.getElementById("fact").textContent = "Verifiable Fact: \"Alice\" is an Aquarius";
-		document.getElementById("fact-source").href = "https://www.instagram.com/noraabravos/";
-		document.getElementById("new1").textContent = "Happy Birthday Alice!";
+	if (d == 30 && m == 6 && y == 2019) {
+		document.getElementById("fact").textContent = "No one has ever been born on July 30th. They have a";
+		document.getElementById("fact-source").href = "https://www.instagram.com/alicegetm/";
+		document.getElementById("new1").textContent = "Happy Birthday Nora!";
 	}
 });
