bd48d2dff9f57182e49cd3099215cbe70c8561b3
astrologyfax <47753594+astrologyfax@users.noreply.github.com>
2019-02-18 16:22:10 -0600

    Update facts.js

--- facts.js
+++ facts.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
 		document.getElementById("fact-source").href = "http://editions.lib.umn.edu/smartpolitics/2010/02/15/presidents-day-special-the-ast/";
 		document.getElementById("new1").textContent = "It's President's Day. Hi Nora";
 	}
-	if (d == 19 && m == 1 && y == 2019)
+	if (d == 19 && m == 1 && y == 2019) {
 		document.getElementById("fact").textContent = "Verifiable Fact: Pisces hate when they get a text saying \"Wussup stranger\"... Like for real!!! phones work both ways";
 		document.getElementById("fact-source").href = "https://21facts.net/interesting-facts-about-pisces/";
 		document.getElementById("new1").textContent = "Happy Birthday Michelle!";
